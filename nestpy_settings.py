2a9b00563016bba51d57fc88fafe499005adc458
cfortan <cfortan@usa.net>
2015-12-26 13:53:28 -0500

    Create nestpy_settings.py

---
+++ nestpy_settings.py
@@ -0,0 +1,6 @@
+## THESE auth urls are pulled from the Nest dev portal. 
+
+nest_auth_uri_1='https://home.nest.com/login/oauth2?client_id=5f249045-cd62-41ea-aa2c-df0af355f79b&state=STATE'
+nest_auth_uri_2='https://api.home.nest.com/oauth2/access_token?client_id=5f249045-cd62-41ea-aa2c-df0af355f79b&code=AUTHORIZATION_CODE&client_secret=BNYeunepmCUEVbwVyYfU0szR2&grant_type=authorization_code'
+
+# http://localhost:5000/oauth2 <- Oauth redirect URL 
